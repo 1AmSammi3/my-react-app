47179e1d9910410fdd3227b8dd09354e54af0836
Sammie <osenisamad2004@gmail.com>
2024-02-08 15:27:11 -0800

    First push to github. I will be sending my changes in on a daily basis now

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './Nav.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <h1>We've got the best movies you can ever imagine</h1>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Nav.css"
+
+export default function Nav() {
+    return(
+        <>
+            <div className='nav'>
+                <img src='src/assets/hamburgerMenu.png'/>
+                <img src='src/assets/logo.png'/>
+            </div>
+        </>
+    )
+}
